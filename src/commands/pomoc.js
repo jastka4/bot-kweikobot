a1b8f092162b423c5620b5df00a523cdf790fa1d
Justyna <jastka4@gmail.com>
2025-01-27 17:00:35 +0100

    Add a basic help command

---
+++ src/commands/pomoc.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder, SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("pomoc")
+    .setDescription("Wyświetla listę dostępnych komend."),
+  async execute(interaction) {
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(0xf1c40f)
+          .setTitle("Pomoc")
+          .addFields(
+            {
+              name: ":computer: Komendy",
+              value:
+                "</pomoc:1333432523128307732> - Wyświetla listę dostępnych komend."
+            },
+          ),
+      ],
+      ephemeral: true,
+    });
+  },
+};
